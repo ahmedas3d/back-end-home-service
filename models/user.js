24d8070632b6ec6a1b4ab6b45b5626fe8d9ea61f
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-03-12 17:02:24 +0200

    update

--- models/user.js
+++ models/user.js
@@ -2,27 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String, // سيتم تخزين رابط الصورة أو مسارها
-      required: false,
-    },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    country: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    image: { type: String, required: false },
   },
   { timestamps: true }
 );
