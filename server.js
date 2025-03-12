08a30ae05361c1642c1bffeecc4010de5f1a421d
deadcodee1 <anaahmedasaad.4@gmail.com>
2025-03-12 15:03:51 +0200

    update server

--- server.js
+++ server.js
@@ -25,6 +25,4 @@ app.use("/api/services", serviceRoutes); // إضافة مسار الخدمات
 
 // تشغيل السيرفر
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
